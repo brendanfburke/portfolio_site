bec0c70bfcdb27ae9a92e02a98145332d300cbc0
brendanburke <bfburke33@gmail.com>
2022-06-16 10:05:35 -0400

    added blank pages and components, ready to make boilerplate pages and routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
